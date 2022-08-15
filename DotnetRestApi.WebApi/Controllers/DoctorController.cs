53b800de256138f141e66f889cd5ee24df950e4a
jke94 <jcarracedo.dev@gmail.com>
2022-08-15 17:25:31 +0200

    Update controller with logging information.

--- DotnetRestApi.WebApi/Controllers/DoctorController.cs
+++ DotnetRestApi.WebApi/Controllers/DoctorController.cs
@@ -19,13 +19,16 @@
     {
         private IApplication<Doctor> _doctor;
         private Mapper _mapper;
+        private ILogger<DoctorController> _logger;
 
         public DoctorController(
             IApplication<Doctor> doctor,
-            Mapper mapper)
+            Mapper mapper,
+            ILogger<DoctorController> logger)
         {
             _doctor = doctor;
             _mapper = mapper;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -37,7 +40,25 @@
         [HttpPost]
         public IActionResult AddMedicalTreatment(DoctorDto doctorDto)
         {
+            if(doctorDto == null)
+            {
+                string message = "Object can not be null.";
+
+                _logger.LogWarning(message);
+
+                return BadRequest(message);
+            }
+
             var element = _mapper.Map<Doctor>(doctorDto);
+
+            if(element == null)
+            {
+                string message = "The structure of the object is not correct.";
+
+                _logger.LogError(message);
+
+                return BadRequest(message);
+            }
 
             return Ok(_doctor.Save(element));
         }
